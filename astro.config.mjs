0a85d723f052212fa1f1d9ee44ca9ab1862c6002
Jean-Philippe Bourdin <jean-philippe_bourdin@MacBook-Air-de-Jean-Philippe.local>
2026-01-31 00:38:35 +0100

    Fix SEO: robots + www canonical

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://confortea.net",
+  site: "https://www.confortea.net",
   integrations: [sitemap()],
 });
